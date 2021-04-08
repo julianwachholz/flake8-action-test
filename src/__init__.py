c635afcab297f7336b04071ed12b55a23033fb85
Julian Wachholz <julian@wachholz.ch>
2021-04-08 13:44:48 +0200

    Add test code

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+# dummy python module
+
+def main():
+    """Really long line that should not be annotated as we're overwriting max line-length with a custom .ini file."""
+    print ("hello")
+
+
+if __name__  == "__main__":
+    main()
