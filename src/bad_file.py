3774b6fa8d9a032b6b6a6da3ed2dc65566011a26
Julian Wachholz <julian@wachholz.ch>
2021-09-18 13:33:16 +0200

    Add very bad file with more than 50 linter errors
    See: https://github.com/julianwachholz/flake8-action/issues/17

---
+++ src/bad_file.py
@@ -0,0 +1,74 @@
+# a file with more than 50 linter errors
+def _bad0(unused, more_unsued):
+  "bad docblock"
+  #incorrect comment
+  while True:
+    pass
+  pass
+  try:
+    out = 1 * 1
+  except:
+    print("too broad")
+  finally:
+    print("finally")
+def _bad1(unused, more_unsued):
+  "bad docblock"
+  #incorrect comment
+  while True:
+    pass
+  pass
+  try:
+    out = 1 * 1
+  except:
+    print("too broad")
+  finally:
+    print("finally")
+
+def _bad2(unused, more_unsued):
+  "bad docblock"
+  #incorrect comment
+  while True:
+    pass
+  pass
+  try:
+    out = 1 * 1
+  except:
+    print("too broad")
+  finally:
+    print("finally")
+def _bad3(unused, more_unsued):
+  "bad docblock"
+  #incorrect comment
+  while True:
+    pass
+  pass
+  try:
+    out = 1 * 1
+  except:
+    print("too broad")
+  finally:
+    print("finally")
+def _bad4(unused, more_unsued):
+  "bad docblock"
+  #incorrect comment
+  while True:
+    pass
+  pass
+  try:
+    out = 1 * 1
+  except:
+    print("too broad")
+  finally:
+    print("finally")
+def _bad5(unused, more_unsued):
+  "bad docblock"
+  #incorrect comment
+  while True:
+    pass
+  pass
+  try:
+    out = 1 * 1
+  except:
+    print("too broad")
+  finally:
+    print("finally")
